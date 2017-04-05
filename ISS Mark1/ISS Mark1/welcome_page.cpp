ca136c1ac816d9768ee0cfb74fc9757d0c51b6f6
Seth Jennings <ash.snaray@gmail.com>
2017-04-05 23:34:36 +0530

    Fixed ortho and coordinates for the last time

--- ISS Mark1/ISS Mark1/welcome_page.cpp
+++ ISS Mark1/ISS Mark1/welcome_page.cpp
@@ -12,7 +12,7 @@ void welcomeScn(void)
 {
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-	gluOrtho2D(-50, 50, -50, 50);	//Values for x and y 
+	gluOrtho2D(-500, 500, -500, 500);	//Values for top, bottom, left and right
 	glMatrixMode(GL_MODELVIEW);
 	glLoadIdentity();
 
@@ -29,54 +29,52 @@ void welcomeScn(void)
 	 *  Diaply all required text on welcome screen
 	 */
 	glColor3f(1.0, 1.0, 1.0);	//Colors to be finalized later
-	dispText(-10, 45, "Srinivas Institute of Technology");
-	dispText(-10, 40, "Valachil, Mangaluru- 574143");
-	dispText(-15, 35, "Department of Computer Science and Engineering");
+	dispText(-102.0, 450.0, "Srinivas Institute of Technology");
+	dispText(-98.0, 400.0, "Valachil, Mangaluru- 574143");
+	dispText(-160.0, 350.0, "Department of Computer Science and Engineering");
 
-	glColor3f(1.0, 1.0, 1.0);
-	dispText(-11, 20, "Computer Graphics and Visualization");
-	dispText(-3, 16, "Mini Project");
-	dispText(-0, 12, "On");
-	dispText(-8, 8, "3D-Environment Exploration");
-	dispText(-8.5, 3, "The International Space Station");
+	dispText(-118.0, 200.0, "Computer Graphics and Visualization");
+	dispText(-38.0, 160.0, "Mini Project");
+	dispText(-10.0, 120.0, "On");
+	dispText(-93.0, 80.0, "3D-Environment Exploration");
+	dispText(-98.0, 30.0, "The International Space Station");
 
-	glColor3f(1.0, 1.0, 1.0);
-	dispText(-46, -20, "Project Guide");
-	dispText(-46, -20, "___________");
-	dispText(-45, -25, "Ms. Vijetha");
-	dispText(-45, -30, "Asst. Prof.");
-	dispText(-45, -35, "CSE Dept.");
-	dispText(29, -20, "Project Coordinator");
-	dispText(29, -20, "_______________");
-	dispText(30, -25, "  Mr. Aravind Naik");
-	dispText(32, -30, "Asst. Prof.");
-	dispText(31.8, -35, " CSE Dept.");
-	dispText(-5, -20, " Team Members");
-	dispText(-5, -20, " ____________");
-	dispText(-9, -25, "Ashweyth Sunil  4SN14CS022");
-	dispText(-9, -30, "Jayasooryan T M  4SN14CS048");
-	dispText(-5, -35, "CSE Dept. 6 CS-A");
+	dispText(-460.0, -200.0, "Project Guide");
+	dispText(-460.0, -200.0, "___________");
+	dispText(-450.0, -250.0, "Ms. Vijetha");
+	dispText(-450.0, -300.0, "Asst. Prof.");
+	dispText(-450.0, -350.0, "CSE Dept.");
+	dispText(290.0, -200.0, "Project Coordinator");
+	dispText(290.0, -200.0, "_______________");
+	dispText(290.0, -250.0, "  Mr. Aravind Naik");
+	dispText(320.0, -300.0, "Asst. Prof.");
+	dispText(315.8, -350, " CSE Dept.");
+	dispText(-58.0, -200.0, " Team Members");
+	dispText(-58.0, -200.0, " ____________");
+	dispText(-92.0, -250.0, "Ashweyth Sunil  4SN14CS022");
+	dispText(-98.0, -300.0, "Jayasooryan T M  4SN14CS048");
+	dispText(-61.7, -350.0, "CSE Dept. 6 CS-A");
 
 	/*
 	 *	Button rendering to move forward to next screen
 	 */
 	glColor3f(1.0, 1.0, 1.0);
-	dispText(-3, -43.5, "Click Here");
+	dispText(-30, -437, "Continue");
 
 	glBegin(GL_POLYGON);
 		glColor3f(0.025, 0.025, 0.75);
-		glVertex2f(-6.7, -40.3);
-		glVertex2f(6.7, -40.3);
-		glVertex2f(6.7, -44.8);
-		glVertex2f(-6.7, -44.8);
+		glVertex2f(-60.7, -400.8);
+		glVertex2f(60.7, -400.8);
+		glVertex2f(60.7, -455.8);
+		glVertex2f(-60.7, -455.8);
 	glEnd();
 
 	glBegin(GL_POLYGON);
 		glColor3f(1.0, 1.0, 1.0);
-		glVertex2f(-7, -40);
-		glVertex2f(7, -40);
-		glVertex2f(7, -45);
-		glVertex2f(-7, -45);
+		glVertex2f(-62, -398);
+		glVertex2f(62, -398);
+		glVertex2f(62, -460);
+		glVertex2f(-62, -460);
 	glEnd();
 }
 
