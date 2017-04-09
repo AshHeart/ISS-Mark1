b5d20d8548a2a26979d802e8d25ac6de70cfc341
Seth Jennings <ash.snaray@gmail.com>
2017-04-10 00:12:41 +0530

    Commenting, commenting, commenting

--- ISS Mark1/ISS Mark1/render.cpp
+++ ISS Mark1/ISS Mark1/render.cpp
@@ -3,6 +3,7 @@
  *  Author: Seth Jennnings <ash.snaray@gmail.com>
  * 
  *  Main rendering and initialization done here and a proceed to the welcome page
+ *  Windows resizing also hendled here
  */
 #include "cus_header.h"
 
@@ -25,8 +26,43 @@ void render(void)
 		break;
 
 	case 2:
-		overview_scn();
+		overviewScn();
+		break;
+
+	case 3:
+		//selectScn();
 		break;
 	}
 	glutSwapBuffers();
+}
+
+void reshape(int w, int h)
+{
+	if (screen == 0) {
+		scnWidth = w;
+		scnHeight = h;
+		glViewport(0, 0, w, h);
+
+		glMatrixMode(GL_PROJECTION);	//Projection Matrix
+		glLoadIdentity();
+
+		glOrtho(-50, 50, -50, 50, -50, 50);	//left, right, bottom, top, near, far
+
+		glMatrixMode(GL_MODELVIEW);		//Modelview Matrix
+		glLoadIdentity();
+	}
+	
+	if (screen == 3) {
+		scnWidth = w;
+		scnHeight = h;
+		glViewport(0, 0, w, h);
+
+		glMatrixMode(GL_PROJECTION);	//Projection Matrix
+		glLoadIdentity();
+
+		gluPerspective(45.0f, (float)w / (float)h, 0.1, 100.0);
+
+		glMatrixMode(GL_MODELVIEW);		//Modelview Matrix
+		glLoadIdentity();
+	}
 }
