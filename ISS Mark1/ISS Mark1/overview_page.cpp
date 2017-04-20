cd599aa02e5b88d2a908fe8d10cfb3bc2f3ce1f0
Seth Jennings <ash.snaray@gmail.com>
2017-04-20 07:49:14 +0530

    Changed rendering to using newer glu objects

--- ISS Mark1/ISS Mark1/overview_page.cpp
+++ ISS Mark1/ISS Mark1/overview_page.cpp
@@ -44,7 +44,7 @@ void overviewScn(void)
 	 *
 	 *	Example: P6 ITS- Integrated Truss Segment Port side 6
 	 */
-	gluLookAt(view[0], view[1], view[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+	gluLookAt(view[0], view[1], view[2], 0.0, 0.0, 0.0, 1.0, 1.0, 0.0);
 	glRotatef(theta[0], 1.0, 0.0, 0.0);
 	glRotatef(theta[1], 0.0, 1.0, 0.0);
 	glRotatef(theta[2], 0.0, 0.0, 1.0);
@@ -62,6 +62,10 @@ void overviewScn(void)
 		glColor3f(0.65, 0.65, 0.65);
 
 		//ST SA Bank 1
+		glPushMatrix();
+		glColor3f(1.0, 0.0, 0.0);
+		gluCylinder(Labs, 1, 1, 0.5, 2, 1);
+		glPopMatrix();
 
 		//ST SA Bank 2
 
@@ -71,7 +75,8 @@ void overviewScn(void)
 
 		//S6 ITS
 		glPushMatrix();
-			glTranslatef(-33.0, 0.0, -150.0);
+		glColor3f(0.65, 0.65, 0.65);
+			glTranslatef(-33.0, 0.0, -50.0);
 			glRotatef(45, 1.0, 0.0, 0.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS, 4, 4, 7, 4, 6);
@@ -79,28 +84,28 @@ void overviewScn(void)
 
 		//S5 ITS
 		glPushMatrix();
-			glTranslatef(-26.0, -0.8, -150.0);
+			glTranslatef(-26.0, -0.8, -50.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS2, 3.5, 3.5, 2, 3, 6);
 		glPopMatrix();
 
 		//S4 ITS
 		glPushMatrix();
-			glTranslatef(-20.6, 0.2, -150.0);
+			glTranslatef(-20.6, 0.2, -50.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			glutSolidCube(5.5);
 		glPopMatrix();
 
 		//S3 ITS
 		glPushMatrix();
-			glTranslatef(-17.6, -0.65, -150.0);
+			glTranslatef(-17.6, -0.65, -50.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS2, 3.5, 3.5, 4, 3, 6);
 		glPopMatrix();
 
 		//S1 ITS
 		glPushMatrix();
-			glTranslatef(-13.5, 0.0, -150.0);
+			glTranslatef(-13.5, 0.0, -50.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS, 3, 3, 7, 5, 10);
 		glPopMatrix();
@@ -109,39 +114,39 @@ void overviewScn(void)
 
 		//Node 1 Unity
 		glPushMatrix();
-			glTranslatef(-3.2, 0.0, -150.0);
+			glTranslatef(-3.2, 0.0, -50.0);
 			gluCylinder(Labs, 3, 3, 5, 60, 10);
 		glPopMatrix();
 
 		//U.S. Lab Destiny
 		glPushMatrix();
-			glTranslatef(-3.2, 0.0, -144.0);
+			glTranslatef(-3.2, 0.0, -44.0);
 			gluCylinder(Labs, 3, 3, 6.5, 60, 10);
 		glPopMatrix();
 
 		//Node 2 Harmony
 		glPushMatrix();
-			glTranslatef(-3.2, 0.0, -138.0);
+			glTranslatef(-3.2, 0.0, -38.0);
 			gluCylinder(Labs, 3, 3, 5, 60, 10);
 		glPopMatrix();
 
  		//JEM Kibo (Hope)
 		glPushMatrix();
-			glTranslatef(-0.0, 0.0, -138.0);
+			glTranslatef(-0.0, 0.0, -38.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(Labs, 3, 3, 7, 60, 10);
 		glPopMatrix();
 
 		//JEM ELM Pressurised Section
 		glPushMatrix();
-			glTranslatef(4.5, 3.0, -138.0);
+			glTranslatef(4.5, 3.0, -38.0);
 			glRotatef(-90, 1.0, 0.0, 0.0);
 			gluCylinder(Labs, 2, 2, 4.5, 60, 10);
 		glPopMatrix();
 
 		//ERL Columbus
 		glPushMatrix();
-			glTranslatef(-6.3, 0.0, -138.0);
+			glTranslatef(-6.3, 0.0, -38.0);
 			glRotatef(-90, 0.0, 1.0, 0.0);
 			gluCylinder(Labs, 3, 3, 5, 60, 10);
 		glPopMatrix();
@@ -151,42 +156,42 @@ void overviewScn(void)
 		//S0 ITS
 		glPushMatrix();
 		glColor3f(0.65, 0.65, 0.65);
-			glTranslatef(0.0, 0.0, -150.0);
+			glTranslatef(0.0, 0.0, -50.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS, 3, 3, 7, 5, 10);
 		glPopMatrix();
 
 		//P1 ITS
 		glPushMatrix();
-			glTranslatef(4.0, 0.0, -150.0);
+			glTranslatef(4.0, 0.0, -50.0);
 			glRotatef(90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS, 3, 3, 7, 5, 10);
 		glPopMatrix();
 
 		//P3 ITS
 		glPushMatrix();
-			glTranslatef(15.0, -0.65, -150.0);
+			glTranslatef(15.0, -0.65, -50.0);
 			glRotatef(-90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS2, 3.5, 3.5, 4, 3, 6);
 		glPopMatrix();
 
 		//P4 ITS
 		glPushMatrix();
-			glTranslatef(18, 0.2, -150.0);
+			glTranslatef(18, 0.2, -50.0);
 			glRotatef(-90, 0.0, 1.0, 0.0);
 			glutSolidCube(5.5);
 		glPopMatrix();
 
 		//P5 ITS
 		glPushMatrix();
-			glTranslatef(23.0, -0.8, -150.0);
+			glTranslatef(23.0, -0.8, -50.0);
 			glRotatef(-90, 0.0, 1.0, 0.0);
 			gluCylinder(ITS2, 3.5, 3.5, 2, 3, 6);
 		glPopMatrix();
 
 		//P6 ITS
 		glPushMatrix();
-		glTranslatef(30.6, 0.0, -150.0);
+		glTranslatef(30.6, 0.0, -50.0);
 		glRotatef(45, 1.0, 0.0, 0.0);
 		glRotatef(-90, 0.0, 1.0, 0.0);
 		gluCylinder(ITS, 4, 4, 7, 4, 6);
