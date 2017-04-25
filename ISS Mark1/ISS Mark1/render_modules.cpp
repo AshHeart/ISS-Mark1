894299d96890d94e7b740bc36be627ec2b4fff3f
Seth Jennings <ash.snaray@gmail.com>
2017-04-25 11:30:00 +0530

    Prelimnary Destiny walls rendered

--- ISS Mark1/ISS Mark1/render_modules.cpp
+++ ISS Mark1/ISS Mark1/render_modules.cpp
@@ -23,6 +23,8 @@ void renderScn(void)
 
 	glEnable(GL_DEPTH_TEST);
 
+	gluLookAt(view[0], view[1], view[2], 0.0, 0.0, 0.0, 0.0, 1.0, 0.0);
+
 	/*
 	 *  Just render node 1 Unity for now
 	 */
@@ -34,9 +36,45 @@ void drawDestiny()
 	//Facing wall
 	glBegin(GL_POLYGON);
 		glColor3f(0.65, 0.65, 0.65);
-		glVertex3f(-10.0, 0.0, -10.0);
-		glVertex3f(-10.0, 10.0, -10.0);
-		glVertex3f(10.0, 10.0, -10.0);
-		glVertex3f(10.0, 0.0, -10.0);
+		glVertex3f(-30.0, 0.0, -50.0);
+		glVertex3f(-30.0, 30.0, -50.0);
+		glVertex3f(30.0, 30.0, -50.0);
+		glVertex3f(30.0, 0.0, -50.0);
+	glEnd();
+
+	//Left Wall
+	glBegin(GL_POLYGON);
+		glColor3f(0.25, 0.25, 0.25);
+		glVertex3f(-30.0, 0.0, -52.0);
+		glVertex3f(-30.0, 30.0, -50.0);
+		glVertex3f(-30.0, 30.0, 10.0);
+		glVertex3f(-30.0, 0.0, 10.0);
+	glEnd();
+
+	//Right wall
+	glBegin(GL_POLYGON);
+		glColor3f(0.25, 0.25, 0.25);
+		glVertex3f(30.0, 30.0, -50.0);
+		glVertex3f(30.0, 0.0, -50.0);
+		glVertex3f(30.0, 0.0, 10.0);
+		glVertex3f(30.0, 30.0, 10.0);
+	glEnd();
+
+	//Top wall
+	glBegin(GL_POLYGON);
+		glColor3f(0.89, 0.89, 0.89);
+		glVertex3f(-30.0, 30.0, -50.0);
+		glVertex3f(30.0, 30.0, -50.0);
+		glVertex3f(30.0, 30.0, 10.0);
+		glVertex3f(-30.0, 30.0, 10.0);
+	glEnd();
+
+	//Floor wall?
+	glBegin(GL_POLYGON);
+		glColor3f(0.89, 0.89, 0.89);
+		glVertex3f(-30.0, 0.0, -50.0);
+		glVertex3f(30.0, 0.0, -50.0);
+		glVertex3f(30.0, 0.0, 10.0);
+		glVertex3f(-30.0, 0.0, 10.0);
 	glEnd();
 }
