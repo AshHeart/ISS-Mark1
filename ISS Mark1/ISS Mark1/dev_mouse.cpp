1cdc505bc59834ee35c180569226fc4e6c0b872f
Seth Jennings <ash.snaray@gmail.com>
2017-05-02 23:00:05 +0530

    Added Camera

--- ISS Mark1/ISS Mark1/dev_mouse.cpp
+++ ISS Mark1/ISS Mark1/dev_mouse.cpp
@@ -1,48 +1,43 @@
 /*
- *  File: dev_mouse.cpp
- *  Author: Seth Jennings <ash.snaray@gmail.com>
- *
- *  File performs as a handler for global mouse events that may and will occur throughout the application 
- *  as interactions on various screens
- */
+*  File: dev_mouse.cpp
+*  Author: Seth Jennings <ash.snaray@gmail.com>
+*
+*  File performs as a handler for global mouse events that may and will occur throughout the application
+*  as interactions on various screens
+*/
 #include "cus_header.h"
 #include <iostream>
 
-float scx, scy;	      //Screen values
-float globx, globy;  //Global values
-float lastx = 0, lasty = 0, lastz = 0;		//Last x, y and z values
-float diffx, diffy, diffz;			//Difference of x, y and zvalues
-float camLrRot, camUdRot;		//Camera rotation Left Right Up Down
-float rotSpeed = 0.2;			//Rotation speed is .2x
+float scX, scY;	                                //Screen values
+float lastX = 0, lastY = 0, lastZ = 0;		   //Last x, y and z values
+float diffX, diffY, diffZ;			          //Difference of x, y and z values
+float camLrRot, camUdRot;		             //Camera rotation Left Right Up Down
+float camX = 1.0, camY = 1.0, camZ = -1.0;  //Camera Postitons
+float rotSpeed = 0.5;			           //Rotation speed is .5x
 
 void mouse(int button, int state, int x, int y)
 {
-	scx = (float(x) / float(scnWidth) - 0.5f) * 100;
-	scy = -(float(y) / float(scnHeight) - 0.5f) * 100;
+	scX = (float(x) / float(scnWidth) - 0.5f) * 100;
+	scY = -(float(y) / float(scnHeight) - 0.5f) * 100;
 
 	/*
-	 *  Initial check for the continue button on the welcpme page
-	 *  in the file welcome_page.cpp
-	 */
-	if (scx >= -6 && scx <= 6 && scy <= -39 && scy >= -45) {
-		if(screen == 1)
+	*  Initial check for the continue button on the welcpme page
+	*  in the file welcome_page.cpp
+	*/
+	if (screen == 1) {
+		if (scX >= -6 && scX <= 6 && scY <= -39 && scY >= -45)
 			screen = 2;
 	}
 
 	/*
-	 *  Check for which part of the station is clicked on the overview
-	 *  in the file overview_page.cpp
-	 */
-	/*if (1) {
-		screen = 3;
-	}*/
-
-	/*
-	 *   To enter the selected module
-	 */
-	if(scx >= 16.0 && scx <= 29.0 && scy <= -32.8 && scy >= -40.9) {
-		if (screen == 3)
+	*   To enter the selected module
+	*/
+	if (screen == 3) {
+		if (scX >= 16.0 && scX <= 29.0 && scY <= -32.8 && scY >= -40.9) {
 			screen = 4;
+			lastX = x;
+			lastY = y;
+		}
 	}
 
 	glutPostRedisplay();
@@ -51,24 +46,20 @@ void mouse(int button, int state, int x, int y)
 void moveMouse(int x, int y)
 {
 	/*
-     *	To register movemet	in module details page
-	 */
+	*	To register movemet	in module details page
+	*/
 	if (screen == 3 || screen == 4) {
 		//Calculate differences in x and y values
-		diffx = lastx - x;
-		diffy = y - lasty;
+		diffX = lastX - x;
+		diffY = y - lastY;
 
 		//Save new values
-		lastx = x;
-		lasty = y;
+		lastX = x;
+		lastY = y;
 
-		camLrRot += diffx * rotSpeed;
-		camUdRot += diffy * rotSpeed;
+		camLrRot += diffX * rotSpeed;
+		camUdRot += diffY * rotSpeed;
 	}
-	else if (screen == 4) {
-		//Calculate difference in x, y and z values
 
-		//Save new values
-	}
 	glutPostRedisplay();
 }
