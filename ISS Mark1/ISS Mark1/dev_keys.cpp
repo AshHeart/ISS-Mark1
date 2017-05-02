3aab8fbaaa4eb61893ae59a510788d3736a57648
Seth Jennings <ash.snaray@gmail.com>
2017-05-02 22:59:39 +0530

    Fixed keys

--- ISS Mark1/ISS Mark1/dev_keys.cpp
+++ ISS Mark1/ISS Mark1/dev_keys.cpp
@@ -1,56 +1,91 @@
 /*
- *  File: dev_keys.cpp
- *  Author: Seth Jennings <ash.snaray@gmail.com>
- *
- *  File implements keyboard functions for the whole application
- *  user will be able to interact with the environment and move around the environment 
- *  using keys on the keyboard
- *  Controls to be appended here after completion
- */
+*  File: dev_keys.cpp
+*  Author: Seth Jennings <ash.snaray@gmail.com>
+*
+*  File implements keyboard functions for the whole application
+*  user will be able to interact with the environment and move around the environment
+*  using keys on the keyboard
+*  Controls to be appended here after completion
+*/
 #include "cus_header.h"
 
-GLint view[] = { 0.0, 0.0, 5.0 };
+GLint view[3] = { 0.0, 0.0, 0.0 };
 GLint axis = 0;
 GLfloat theta[3] = { 0.0, 0.0, 0.0 };
+bool specKey[255];
 
 /*
- *  Global keyboard interaction and events handler
- */
+*  Global keyboard interaction and events handler
+*/
 void keyboard(unsigned char key, int x, int y) {
 	/*
-	 *  If ESC is pressed give the user an option to quit out of the application from anywhere
-	 *  decision if just text is needed or a whole new window will bw experimanted on this branch
-	 */
+	*  If ESC is pressed give the user an option to quit out of the application from anywhere
+	*  decision if just text is needed or a whole new window will bw experimanted on this branch
+	*/
 	if (key == 27) {
 		exit(0);
 	}
 
-	if (key == 'c' && screen == 2) {
-			screen = 3;
+	/*
+	*  IF c is pressed go inside the room
+	*/
+	if (screen == 2) {
+		if (key == 'c' || key == 'C') {
+			screen = 4;
+		}
 	}
 
-	if (screen == 2 || screen == 3 || screen == 4) {
-		if (key == 's')	view[1] += 1;	  //y view
-		if (key == 'a') view[0] += 1;	 //x view
-		if (key == 'w')	view[2] += 1;	//z view
+	/*
+	*  While inside the room, if i is
+	*  pressed show information about the module
+	*/
+	if (screen == 4) {
+		if (key == 'i' || key == 'I') {
+			screen = 3;
+		}
+	}
 
-		if (key == 'f') view[1] -= 1;     //-y view
+	/*
+	*  After displaying information is b
+	*	is pressed go back inside the room
+	*/
+	if (screen == 3) {
+		if (key == 'b' || key == 'B') {
+			screen = 4;
+		}
+	}
+
+	if (screen == 2) {
+		if (key == 'w')	view[1] += 1;	  //y view
+		if (key == 'a') view[0] += 1;	 //x view
+		if (key == 'j')	view[2] += 1;	//z view
+
+		if (key == 's') view[1] -= 1;     //-y view
 		if (key == 'd') view[0] -= 1;    //-x view
-		if (key == 'r') view[2] -= 1;	//-z view
-		
-		if (key == 'v') axis = 0;
-		if (key == 'b') axis = 1;
-		if (key == 'n') axis = 2;
-		
+		if (key == 'l') view[2] -= 1;	//-z view
+
 		theta[axis] += 1.0;
 		if (theta[axis] > 360.0) theta[axis] -= 360.0;
 		overviewScn();
 	}
 
-	if (key == 27 && screen == 1 || screen == 2 || screen == 3 || screen == 4) {
-		screen = 5;
+	glutPostRedisplay();
+}
+
+void specKeys(int key, int x, int y)
+{
+	if (screen == 4) {
+		specKey[key] = true;
 	}
 
 	glutPostRedisplay();
 }
 
+void specKeysUp(int key, int x, int y)
+{
+	if (screen == 4) {
+		specKey[key] = false;
+	}
+
+	glutPostRedisplay();
+}
