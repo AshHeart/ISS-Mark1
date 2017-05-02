653aeec18e4e8cd40d7cf521a743871d31209924
Seth Jennings <ash.snaray@gmail.com>
2017-05-02 22:59:19 +0530

    Fixed camera finally

--- ISS Mark1/ISS Mark1/dev_camera.cpp
+++ ISS Mark1/ISS Mark1/dev_camera.cpp
@@ -1,19 +1,49 @@
 /*
- *	File: dev_camera.cpp
- *	Author: Seth Jennings <ash.snaray@gmail.com>
- *
- *  Handle all camera functions including rotation of outer view, details view 
- *	and inside the modules
- */
+*	File: dev_camera.cpp
+*	Author: Seth Jennings <ash.snaray@gmail.com>
+*
+*  Handle all camera functions including rotation of outer view, details view
+*	and inside the modules
+*/
 #include "cus_header.h"
 
-float transAng;
+float transAng, transX, transY, transZ;
 
 void cameraControl()
 {
-	transAng = 630 - camUdRot;
 
-	//glRotatef(camLrRot, 1.0, 0.0, 0.0);
-	
-	glRotatef(transAng, 0.0, 1.0, 0.0);
+	if (specKey[GLUT_KEY_UP]) {
+		camZ -= 0.1;
+	}
+	else if (specKey[GLUT_KEY_DOWN]) {
+		camZ += 0.1;
+	}
+	else if (specKey[GLUT_KEY_LEFT]) {
+		camX -= 0.1;
+	}
+	else if (specKey[GLUT_KEY_RIGHT]) {
+		camX += 0.1;
+	}
+	else if (specKey[GLUT_KEY_ALT_R]) {
+		camY -= 0.1;
+	}
+	else if (specKey[GLUT_KEY_CTRL_R]) {
+		camY += 0.1;
+	}
+
+	transX = -camX;
+	transY = -camY;
+	transZ = -camZ;
+
+	if (camLrRot > 360)
+		camLrRot -= 360;
+	else if (camLrRot < 0)
+		camLrRot += 360;
+
+	transAng = 360 - camUdRot;
+
+	glRotatef(camLrRot, 0.0, 1.0, 0.0);
+	glRotatef(transAng, 1.0, 0.0, 0.0);
+
+	glTranslatef(transX, transY, transZ);
 }
