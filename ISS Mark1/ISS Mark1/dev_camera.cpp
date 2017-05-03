501c57f9d03a907494c0d64c02341a5b1a44e005
Seth Jennings <ash.snaray@gmail.com>
2017-05-03 08:25:26 +0530

    Added collisions

--- ISS Mark1/ISS Mark1/dev_camera.cpp
+++ ISS Mark1/ISS Mark1/dev_camera.cpp
@@ -6,23 +6,50 @@
 *	and inside the modules
 */
 #include "cus_header.h"
+#define PI_OVER_180  0.0174532925f		//PI value
 
 float transAng, transX, transY, transZ;
+
+bool noCollision();
 
 void cameraControl()
 {
 
 	if (specKey[GLUT_KEY_UP]) {
-		camZ -= 0.1;
+		camX -= (float)sin(camLrRot * PI_OVER_180) * 0.1;
+		camZ -= (float)cos(camLrRot * PI_OVER_180) * 0.1;
+
+		if (!noCollision()) {
+			camX += (float)sin(camLrRot * PI_OVER_180) * 0.1;
+			camZ += (float)cos(camLrRot * PI_OVER_180) * 0.1;
+		}
 	}
 	else if (specKey[GLUT_KEY_DOWN]) {
-		camZ += 0.1;
+		camX += (float)sin(camLrRot * PI_OVER_180) * 0.1;
+		camZ += (float)cos(camLrRot * PI_OVER_180) * 0.1;
+
+		if (!noCollision()) {
+			camX -= (float)sin(camLrRot * PI_OVER_180) * 0.1;
+			camZ -= (float)cos(camLrRot * PI_OVER_180) * 0.1;
+		}
 	}
 	else if (specKey[GLUT_KEY_LEFT]) {
-		camX -= 0.1;
+		camX -= (float)cos((360 - camLrRot) * PI_OVER_180) * 0.1;
+		camZ -= (float)sin((360 - camLrRot) * PI_OVER_180) * 0.1;
+
+		if (!noCollision()) {
+			camX += (float)cos((360 - camLrRot) * PI_OVER_180) * 0.1;
+			camZ += (float)sin((360 - camLrRot) * PI_OVER_180) * 0.1;
+		}
 	}
 	else if (specKey[GLUT_KEY_RIGHT]) {
-		camX += 0.1;
+		camX += (float)cos((360 - camLrRot) * PI_OVER_180) * 0.1;
+		camZ += (float)sin((360 - camLrRot) * PI_OVER_180) * 0.1;
+
+		if (!noCollision()) {
+			camX -= (float)cos((360 - camLrRot) * PI_OVER_180) * 0.1;
+			camZ -= (float)sin((360 - camLrRot) * PI_OVER_180) * 0.1;
+		}
 	}
 	else if (specKey[GLUT_KEY_ALT_R]) {
 		camY -= 0.1;
@@ -46,4 +73,39 @@ void cameraControl()
 	glRotatef(transAng, 1.0, 0.0, 0.0);
 
 	glTranslatef(transX, transY, transZ);
+}
+
+bool noCollision() {
+	if (camX < 5.0 && camX > -5) {	//Inside destiny
+		if (camZ < -5 && camZ > 1) {
+			if (camY < 5 && camY > 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+
+	if (camX < 1 && camX > -3) {	//Between Harmony and Destiny
+		if (camZ < -9 && camZ > -6) {
+			if (camY < -6 && camY > -5) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
+	
+	if (camX < 5.0 && camX > -5) {	//Inside harmony
+		if (camZ < -9 && camZ > -6) {
+			if (camY < 5 && camY > 0) {
+				return true;
+			}
+			else {
+				return false;
+			}
+		}
+	}
 }
